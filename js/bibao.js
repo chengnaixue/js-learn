163b86e9661bfd101bc6fe2f8c7df3006c8ddd06
chengnaixue <chengnaixue@gmail.com>
2015-03-30 21:43:00 +0800

    1.闭包与变量

--- js/bibao.js
+++ js/bibao.js
@@ -1,7 +1,38 @@
+/**
+	1.闭包与变量
+*/
 function createFunctions(){
-	
+	var result = new Array();
+
+	for(var i=0; i<10; i++){
+		result[i] = function(){
+			return i;
+		};
+	}
+
+	return result;
 }
 
-function(){
+function createFunctions2(){
+	var result = new Array();
 
-}();
+	for(var i=0; i<10; i++){
+		result[i] = function(num){
+			return function(){
+				return num;
+			};
+		}(i);
+	}
+
+	return result;
+}
+
+/**
+	2.
+*/
+(function(){
+	var j = 5;
+	alert(j);
+
+})();
+
